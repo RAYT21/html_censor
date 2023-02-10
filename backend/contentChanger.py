28ddd54bf67ea49da98d9898fdd84d7e1b24bb45
Павел Капустин <pkapustin@vtb.ru>
2023-02-10 12:27:03 +0300

    добавлены новые версии питона и требуемой библиотеки, созданы папки для кода питона, начато продумываение как менять текст в приходящем html

---
+++ backend/contentChanger.py
@@ -0,0 +1,28 @@
+import enum
+import re
+
+
+class ContentChanger:
+
+    def changePage(json):
+        return json
+    
+    def changeDOMelement():
+        return
+
+    def readSettings():
+        return
+
+class PullSettings():
+
+    def __init__(self):         
+        self.typeHidding 
+        self.regexAnalys
+        self.modelAnalys
+
+    def setSettings():
+        return
+
+
+     
+
---
+++ frontend/js/inject_scripts.js
@@ -0,0 +1,13 @@
+
+// подгрузить на используемую страницу css, отправить на обработку html найти на нем запрещенку,
+// изменить это на сервере и передать обратно, интегрировать полученный html в исходный
+
+function injectHTML(){
+    let injectCSS='/frontend/css/inject_styles.css';
+    let text= '';
+    let hoverText='';
+    let hidden_setting='<span class="inject-text" text="DEFAULT TEXT" hover-text="NEW TEXT FROM ATTRIBUTE"></span>';
+
+}
+
+<span class="inject-text" text="DEFAULT TEXT" hover-text="NEW TEXT FROM ATTRIBUTE"></span>
