8f63fc7dadca090a404306429b0f125593dac3d2
Дмитрий Щербаков <thelegodorkdima@gmail.com>
2023-02-12 03:14:16 +0300

    Изменил метод запроса на ассинхронный

--- frontend/js/dictionary.js
+++ frontend/js/dictionary.js
@@ -13,58 +13,59 @@ document.addEventListener('DOMContentLoaded', () => {
 
     plusBtn.addEventListener('click', (e) => {
         e.preventDefault();
+        addWord();
+    });
+    function addWord(){
         if (wordInput.value != '') {
             const wordBox = document.createElement('div');
             wordBox.classList.add('logField', 'word');
             console.log(wordInput.value);
-
-            const hxr = requestToServer(wordInput.value);
-            if (hxr.status == 200) {
-                wordBox.textContent = wordInput.value;
-                wordsParent.append(wordBox);
-            }
+            const word = wordInput.value;
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', 'http://127.0.0.1:5000/regexGenerator/'+wordInput.value, true);
+    
+            xhr.onload = (e) => {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        console.log(xhr.responseText);
+                        wordBox.textContent = word;
+                        wordsParent.append(wordBox);
         
-            wordInput.value = '';
+                        wordInput.value = '';
 
-
-
-            wordBox.addEventListener('mouseenter', () => {
-               wordBox.classList.add('red');
-            });
-
-            wordBox.addEventListener('mouseleave', () => {
-                wordBox.classList.remove('red');
-             });
-
-            wordBox.addEventListener('click', () => {
-                wordBox.remove();
-            });
+                        wordBox.addEventListener('mouseenter', () => {
+                            wordBox.classList.add('red');
+                        });
+                        wordBox.addEventListener('mouseleave', () => {
+                            wordBox.classList.remove('red');
+                        });
+                        wordBox.addEventListener('click', () => {
+                            wordBox.remove();
+                        });
+                    }
+                    else {
+                        console.error(xhr.statusText);
+                    }
+                }
+            };
+            xhr.onerror = (e) => {
+                console.error(xhr.statusText);
+                alert("Слово - <" + word + "> не было создано - сервер не отвечает.")
+            };
+            xhr.send();
+            
 
         } else {
             console.log('ne ok');
         }
-    });
-
+    }
     backMenuBtn.addEventListener('click', (e) => {
         e.preventDefault();
         window.location.href = '/frontend/html/menu.html';
     });
 });
 
-function requestToServer(word){
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://127.0.0.2:5000/regexGenerator/'+word, false);
-  xhr.send();
 
-  if (xhr.status != 200) {
-    console.log(xhr.status + ': ' + xhr.statusText);
-  } else {
-    console.log(xhr.status + ': ' + xhr.statusText);
-    console.log(xhr.response)
-    return xhr;
-  }
-  
-} 
 
 function parseJsonByRegex(){
     
